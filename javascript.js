ff594c56fcfc244d36b1a4e7852a7177fa8c7c2f
Dragos Stanciu <stanciudragos1337@gmail.com>
2022-08-25 12:45:40 +0300

    Create getPlayerChoice function
    This function asks the user to enter a choice and stores the result

--- javascript.js
+++ javascript.js
@@ -2,3 +2,7 @@ function getComputerChoice() {
     const choices = ['Rock', 'Papper', 'Scissors']
     return (choices[Math.floor(choices.length * Math.random())]);
 }
+
+function getPlayerChoice() {
+    return prompt('Rock, Papper or Scissors?')
+}
